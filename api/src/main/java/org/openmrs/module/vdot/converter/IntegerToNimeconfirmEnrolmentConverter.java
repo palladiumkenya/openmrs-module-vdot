cfeb873495906f36aaee80a3e8f54a8ab5b672cd
Stephen Chege <njorocs@gmail.com>
2021-05-13 14:44:17 +0300

    Cleaning up code

--- api/src/main/java/org/openmrs/module/vdot/converter/IntegerToNimeconfirmEnrolmentConverter.java
+++ api/src/main/java/org/openmrs/module/vdot/converter/IntegerToNimeconfirmEnrolmentConverter.java
@@ -1,7 +1,28 @@
 package org.openmrs.module.vdot.converter;
 
+import org.openmrs.api.context.Context;
+import org.openmrs.module.vdot.api.NimeconfirmEnrolment;
+import org.openmrs.module.vdot.api.NimeconfirmService;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
 /**
- * Created by Steve on 13 May, 2021
+ * Created by Palladium Dev on 13 May, 2021
  */
-public class IntegerToNimeconfirmEnrolmentConverter {
+@Component
+public class IntegerToNimeconfirmEnrolmentConverter implements Converter<Integer, NimeconfirmEnrolment> {
+
+    /**
+     * @see Converter#convert(Object)
+     */
+    @Override
+    public NimeconfirmEnrolment convert(Integer id) {
+        NimeconfirmService service = Context.getService(NimeconfirmService.class);
+        if (id == null) {
+            return null;
+        } else {
+            return service.getNimeconfirmEnrolmentByID(id);
+        }
+
+    }
 }
--- api/src/main/java/org/openmrs/module/vdot/converter/IntegerToNimeconfirmVideoObsConverter.java
+++ api/src/main/java/org/openmrs/module/vdot/converter/IntegerToNimeconfirmVideoObsConverter.java
@@ -1,30 +1,28 @@
 package org.openmrs.module.vdot.converter;
 
 import org.openmrs.api.context.Context;
-import org.openmrs.module.vdot.api.NimeconfirmEnrolment;
 import org.openmrs.module.vdot.api.NimeconfirmService;
+import org.openmrs.module.vdot.api.NimeconfirmVideoObs;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by Palladium Dev on 13 May, 2021
  */
-public class IntegerToNimeconfirmEnrolmentConverter {
     @Component
-    public class IntegerToNimeconfirmVideoObsConverter implements Converter<Integer, NimeconfirmEnrolment> {
+    public class IntegerToNimeconfirmVideoObsConverter implements Converter<Integer, NimeconfirmVideoObs> {
 
         /**
          * @see Converter#convert(Object)
          */
         @Override
-        public NimeconfirmEnrolment convert(Integer id) {
+        public NimeconfirmVideoObs convert(Integer id) {
             NimeconfirmService service = Context.getService(NimeconfirmService.class);
             if (id == null) {
                 return null;
             } else {
-                return service.getNimeconfirmEnrolmentByID(id);
+                return service.getNimeconfirmVideoObsByID(id);
             }
 
         }
     }
-}
