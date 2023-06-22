38257edf8e37c58d30f88732e99907a0e55a87ec
jecihjoy <jecihjoy@gmail.com>
2023-06-22 11:30:09 +0300

    Add support for platform 2.6.x

--- api/src/main/java/org/openmrs/module/vdot/metadata/VdotSecurityMetadata.java
+++ api/src/main/java/org/openmrs/module/vdot/metadata/VdotSecurityMetadata.java
@@ -14,26 +14,26 @@ import static org.openmrs.module.metadatadeploy.bundle.CoreConstructors.role;
 @Component
 @Requires(org.openmrs.module.kenyaemr.metadata.SecurityMetadata.class)
 public class VdotSecurityMetadata extends AbstractMetadataBundle {
-
-    public static class _Privilege {
-        public static final String APP_NIMECONFIRM_ADMIN = "App: vdot.vdot";
-    }
-
-    public static final class _Role {
-        public static final String APPLICATION_NIMECONFIRM_ADMIN = "nimeCONFIRM app administration";
-    }
-
-    /**
-     * @see AbstractMetadataBundle#install()
-     */
-    @Override
-    public void install() {
-
-        install(privilege(_Privilege.APP_NIMECONFIRM_ADMIN, "Able to administer nimeCONFIRM actions"));
-        install(role(_Role.APPLICATION_NIMECONFIRM_ADMIN, "Can access nimeCONFIRM app", idSet(
-                org.openmrs.module.kenyaemr.metadata.SecurityMetadata._Role.API_PRIVILEGES_VIEW_AND_EDIT
-        ), idSet(
-                _Privilege.APP_NIMECONFIRM_ADMIN
-        )));
-    }
+	
+	public static class _Privilege {
+		
+		public static final String APP_NIMECONFIRM_ADMIN = "App: vdot.vdot";
+	}
+	
+	public static final class _Role {
+		
+		public static final String APPLICATION_NIMECONFIRM_ADMIN = "nimeCONFIRM app administration";
+	}
+	
+	/**
+	 * @see AbstractMetadataBundle#install()
+	 */
+	@Override
+	public void install() {
+		
+		install(privilege(_Privilege.APP_NIMECONFIRM_ADMIN, "Able to administer nimeCONFIRM actions"));
+		install(role(_Role.APPLICATION_NIMECONFIRM_ADMIN, "Can access nimeCONFIRM app",
+		    idSet(org.openmrs.module.kenyaemr.metadata.SecurityMetadata._Role.API_PRIVILEGES_VIEW_AND_EDIT),
+		    idSet(_Privilege.APP_NIMECONFIRM_ADMIN)));
+	}
 }
